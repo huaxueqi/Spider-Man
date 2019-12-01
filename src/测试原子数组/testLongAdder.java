d89cc35c5a3a0d81ffd9eea7f4296215ed00154e
huangrh <huangrh@ziroom.com>
2019-12-01 17:51:11 +0800

    提交

--- src/测试原子数组/testLongAdder.java
+++ src/测试原子数组/testLongAdder.java
@@ -8,12 +8,15 @@ public class testLongAdder {
     public static void main(String[] args) {
           longAdder.add(10);
           longAdder.increment();
-          System.out.println(longAdder.intValue());
-          System.out.println("_____________________");
+         /* System.out.println(longAdder.intValue());
+          System.out.println("_____________________");*/
      /*     Date d=new Date();
         SimpleDateFormat simpleDateFormat=new SimpleDateFormat();*/
-          System.out.println(a++);
-
+          //System.out.println(a++);
+         Integer a=new Integer(255);
+        Integer b=new Integer(255);
+        System.out.println(a==b);
+        System.out.println(a.equals(b));
 
     }
 }
