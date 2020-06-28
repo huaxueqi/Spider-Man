7af1b3310bb129f055d2bce9c09e204a4d90e6dd
huaxueqi <hxq13449>
2020-06-28 11:11:15 +0800

    提交

---
+++ src/BIGdesimalText.java
@@ -0,0 +1,25 @@
+import java.math.BigDecimal;
+
+public class BIGdesimalText {
+    public static void main(String[] args) {
+        BigDecimal num = new BigDecimal("2.456987");
+        System.out.println(num.scale());
+        BigDecimal b = new BigDecimal("2.225667").setScale(2, BigDecimal.ROUND_DOWN);
+        System.out.println(b);
+        BigDecimal C = new BigDecimal("2.220667").setScale(2, BigDecimal.ROUND_UP);
+        System.out.println(C);
+        BigDecimal f = new BigDecimal("2.224667").setScale(2, BigDecimal.ROUND_CEILING);
+        System.out.println(f);//2.23 如果是正数，相当于BigDecimal.ROUND_UP
+
+        BigDecimal g = new BigDecimal("-2.225667").setScale(2, BigDecimal.ROUND_CEILING);
+        System.out.println(g);//-2.22 如果是负数，相当于BigDecimal.ROUND_DOWN
+
+
+        BigDecimal l = new BigDecimal("2.2856").setScale(4, BigDecimal.ROUND_UNNECESSARY);
+        System.out.println(l);
+        //断言请求的操作具有精确的结果，因此不需要舍入。
+        //如果对获得精确结果的操作指定此舍入模式，则抛出ArithmeticException。
+
+
+    }
+}
---
+++ src/docker.java
@@ -0,0 +1,29 @@
+import com.sun.deploy.net.proxy.*;
+
+import java.net.URL;
+
+public class docker implements ProxyHandler {
+    int a(int b) {
+        return b;
+    }
+
+    @Override
+    public boolean isSupported(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean isProxyCacheSupported() {
+        return false;
+    }
+
+    @Override
+    public void init(BrowserProxyInfo browserProxyInfo) throws ProxyConfigException {
+
+    }
+
+    @Override
+    public ProxyInfo[] getProxyInfo(URL url) throws ProxyUnavailableException {
+        return new ProxyInfo[0];
+    }
+}
