010af1d45381043040b985aae5c2288664d6923e
huaxueqi <hxq13449>
2020-09-01 14:17:44 +0800

    提交

---
+++ src/自动释放/demo1.java
@@ -0,0 +1,25 @@
+package 自动释放;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class demo1 {
+    public static void main(String[] args) {
+           File one =new File("E:\\Spider-Man\\a.txt");
+           demo1.filyCopy2(one);
+    }
+    public static void filyCopy2(File one){
+        try (FileInputStream fileInput = new FileInputStream(one);
+             FileOutputStream fileOutput = new FileOutputStream("E:\\Spider-Man\\b.txt")
+        ){
+            byte[] b = new byte[1024];
+            int len = 0;
+            while((len = fileInput.read(b)) != -1){
+                fileOutput.write(b, 0, len);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
