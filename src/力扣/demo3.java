e904c6f828ac1affd06afe1e0a117cd3257db481
huaxueqi <hxq13449>
2020-09-16 14:11:07 +0800

    commit

--- src/力扣/demo3.java
+++ src/力扣/demo3.java
@@ -1,10 +1,14 @@
 package 力扣;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class demo3 {
+    /**
+     * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates
+     * 中所有可以使数字和为 target 的组合。
+     * candidates 中的数字可以无限制重复被选取。
+
+     */
     private List<Integer> list = new LinkedList<>();
     private List<List<Integer>> res = new LinkedList<>();
     private int n;
@@ -33,6 +37,45 @@ public class demo3 {
         solve(index + 1, sum, target, a);
     }
     public static void main(String[] args) {
+        demo3 solution = new demo3();
+        int[] candidates = new int[]{2, 3, 6, 7};
+        int target = 7;
+        List<List<Integer>> res = solution.combinationSum1(candidates, target);
+        System.out.println("输出 => " + res);
 
     }
+
+    public List<List<Integer>> combinationSum1(int[] candidates, int target) {
+        int len = candidates.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+
+        Arrays.sort(candidates);
+        Deque<Integer> path = new ArrayDeque<>();
+        dfs(candidates, 0, len, target, path, res);
+        return res;
+    }
+
+    private void dfs(int[] candidates, int begin, int len, int target, Deque<Integer> path, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = begin; i < len; i++) {
+            if (target - candidates[i] < 0) {
+                break;
+            }
+
+            path.addLast(candidates[i]);
+            System.out.println("递归之前 => " + path + "，剩余 = " + (target - candidates[i]));
+
+            dfs(candidates, i, len, target - candidates[i], path, res);
+            path.removeLast();
+            System.out.println("递归之后 => " + path);
+        }
+    }
+
 }
---
+++ src/力扣/demo4.java
@@ -0,0 +1,61 @@
+package 力扣;
+
+import shejimoshi.组合模式.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class demo4 {
+    //遍历二叉树的所有路径
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(4);
+        TreeNode node4 = new TreeNode(5);
+        TreeNode node5 = new TreeNode(6);
+        TreeNode node6 = new TreeNode(7);
+        TreeNode node7 = new TreeNode(8);
+        root.left=node1;
+        root.right=node2;
+        node1.left=node3;
+        node1.right=node4;
+        node2.left=node5;
+        node2.right=node6;
+        node3.left=node7;
+        demo4.binaryTreePaths(root).forEach(l->{
+            System.out.println(l);
+        });
+
+
+    }
+
+    public static List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new LinkedList<>();
+        if (root == null)
+            return res;
+        //到达叶子节点，把路径加入到集合中
+        if (root.left == null && root.right == null) {
+            res.add(root.val + "");
+            return res;
+        }
+        //遍历左子节点的路径
+        for (String path : binaryTreePaths(root.left)) {
+            res.add(root.val + "->" + path);
+        }
+        //遍历右子节点的路径
+        for (String path : binaryTreePaths(root.right)) {
+            res.add(root.val + "->" + path);
+        }
+        return res;
+    }
+
+    static class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
