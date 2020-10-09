dbca976761f1ffce0e4a889bfaa36ca9981602b0
huaxueqi <hxq13449>
2020-10-09 13:59:29 +0800

    commit

--- src/剑指/eighteen.java
+++ src/剑指/eighteen.java
@@ -1,21 +1,33 @@
 package 剑指;
 
+import 信号量.Demo5;
+
 /**
  * 输入一个二叉树A,B。判断A是否为B的子结构，空树不为任何一个树的子结构
  */
 public class eighteen {
     //
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode r1 = new TreeNode(2);
+        TreeNode r2 = new TreeNode(3);
+        TreeNode r3 = new TreeNode(4);
+        TreeNode r4 = new TreeNode(5);
+        TreeNode r5 = new TreeNode(6);
+
+
     }
-   public boolean HasSubtree(TreeNode root1,TreeNode root2) {
-        if(root1 == null || root2 == null) return  false;
-        return  HasSubtree(root1.left,root2) || HasSubtree(root1.right,root2) ||
-                jump(root1,root2);
+
+    public boolean HasSubtree(TreeNode root1, TreeNode root2) {
+        if (root1 == null || root2 == null) return false;
+        return HasSubtree(root1.left, root2) || HasSubtree(root1.right, root2) ||
+                jump(root1, root2);
     }
-    public boolean jump(TreeNode t1,TreeNode t2){
-        if(t2 == null) return true;
-        if(t1 == null) return false;
-        if(t1.val != t2.val)  return false;
-        return jump(t1.left,t2.left) && jump(t1.right,t2.right);
+
+    public boolean jump(TreeNode t1, TreeNode t2) {
+        if (t2 == null) return true;
+        if (t1 == null) return false;
+        if (t1.val != t2.val) return false;
+        return jump(t1.left, t2.left) && jump(t1.right, t2.right);
     }
 }
--- src/力扣/demo7.java
+++ src/力扣/demo7.java
@@ -4,27 +4,30 @@ package 力扣;
 public class demo7 {
     //实现一种算法，找出单向链表中倒数第 k 个节点。返回该节点的值。
     public static void main(String[] args) {
-        ListNode l1=new ListNode(1);
-        ListNode l2=new ListNode(2);
-        ListNode l3=new ListNode(3);
-        ListNode l4=new ListNode(4);
-        ListNode l5=new ListNode(5);
-        l1.next=l2;
-        l2.next=l3;
-        l3.next=l4;
-        l4.next=l5;
-        int k=6;
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = l6;
+        int k = 6;
         /*Integer Q= demo7.kthToLast(l1,k);
         if( Q == null)
             System.out.println("为空");
         else
             System.out.println(Q);*/
-       ListNode Q1= demo7.kthToLast1(l1);
-        while( Q1 != null){
-            System.out.print(Q1.val+"   ");
-            Q1= Q1.next;
+        ListNode Q1 = demo7.kthToLast(l1);
+        while (Q1 != null) {
+            System.out.print(Q1.val + "   ");
+            Q1 = Q1.next;
         }
     }
+
     public static Integer kthToLast(ListNode head, int k) {
         ListNode p, q;
         p = q = head;
@@ -37,15 +40,16 @@ public class demo7 {
         return i < k ? null : q.val;
     }
 
-    public static ListNode kthToLast1(ListNode head) {
+    //删除链表中间的节点
+    public static ListNode kthToLast(ListNode head) {
         ListNode p, q;
         p = head;
-        q=head.next.next;
-        while(q.next !=null && q.next.next !=null){
-            p=p.next;
-            q=q.next.next;
+        q = head.next.next;
+        while (q.next != null && q.next.next != null) {
+            p = p.next;
+            q = q.next.next;
         }
-        p.next=p.next.next;
+        p.next = p.next.next;
         return head;
     }
 }
