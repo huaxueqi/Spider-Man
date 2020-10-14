37d7b685ef34a05b4548dfe9ed720dc5ba0dd962
huaxueqi <hxq13449>
2020-10-14 08:52:05 +0800

    commit

--- src/剑指/twenty_six.java
+++ src/剑指/twenty_six.java
@@ -8,22 +8,52 @@ public class twenty_six {
     public static void main(String[] args) {
 
     }
-    TreeNode pre=null;
-    TreeNode head=null;
-    public TreeNode convert(TreeNode root){
-        if (root == null){
+
+    TreeNode pre = null;
+    TreeNode head = null;
+
+    public TreeNode convert(TreeNode root) {
+        if (root == null) {
             return null;
         }
         convert(root.left);
-        root.left=pre;
-        if(pre!=null){
-            pre.right=root;
+        root.left = pre;
+        if (pre != null) {
+            pre.right = root;
         }
-        pre=root;
-        if(head==null){
-            head=root;
+        pre = root;
+        if (head == null) {
+            head = root;
         }
         convert(root.right);
         return head;
     }
 }
+   /**
+    Node head, pre;
+    public Node treeToDoublyList(Node root) {
+        if(root==null) return null;
+        dfs(root);
+
+        pre.right = head;
+        head.left =pre;//进行头节点和尾节点的相互指向，这两句的顺序也是可以颠倒的
+
+        return head;
+
+    }
+
+    public void dfs(Node cur){
+        if(cur==null) return;
+        dfs(cur.left);
+
+        //pre用于记录双向链表中位于cur左侧的节点，即上一次迭代中的cur,当pre==null时，cur左侧没有节点,即此时cur为双向链表中的头节点
+        if(pre==null) head = cur;
+            //反之，pre!=null时，cur左侧存在节点pre，需要进行pre.right=cur的操作。
+        else pre.right = cur;
+
+        cur.left = pre;//pre是否为null对这句没有影响,且这句放在上面两句if else之前也是可以的。
+
+        pre = cur;//pre指向当前的cur
+        dfs(cur.right);//全部迭代完成后，pre指向双向链表中的尾节点
+    }
+    */
