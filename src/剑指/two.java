575e51008f6a46fd2ca19014d9034ef975d527dd
huaxueqi <hxq13449>
2020-10-23 11:34:58 +0800

    commit

--- src/剑指/two.java
+++ src/剑指/two.java
@@ -22,4 +22,19 @@ public class two {
         }
       return new String(buffer);
     }
+
+
+    public String replaceSpace(String s) {
+        if(s == null)
+            return null;
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+                sb.append("%20");
+            }else{
+                sb.append(s.charAt(i));
+            }
+        }
+        return  new String(sb);
+    }
 }
