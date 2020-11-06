44fe7439f46f2dcf4ae046bddf8bf8ceaf980105
huaxueqi <hxq13449>
2020-11-06 08:55:12 +0800

    commit

--- src/力扣/接雨水.java
+++ src/力扣/接雨水.java
@@ -9,7 +9,7 @@ public class 接雨水 {
      */
     public static void main(String[] args) {
         int[] a = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        System.out.println(接雨水.trap(a));
+        System.out.println(接雨水.jiehuafa(a));
     }
 
     static int trap(int[] height) {
@@ -26,11 +26,30 @@ public class 接雨水 {
 
             // ans += min(l_max, r_max) - height[i]
             if (l_max < r_max) {
-                ans += l_max - height[left];
+                ans += l_max - height[left];  //接多少雨水，取决于最矮的柱子
                 left++;
             } else {
                 ans += r_max - height[right];
                 right--;
+            }
+        } 
+        return ans;
+    }
+
+    static int jiehuafa(int[] a) {
+        if (a.length == 0) return 0;
+        int i = 0, j = a.length - 1;
+        int ans = 0;
+        int lmax = a[i], rmax = a[j];
+        while (i <= j) {
+            lmax = max(lmax, a[i]);
+            rmax = max(rmax, a[j]);
+            if (lmax < rmax) {
+                ans += lmax - a[i];
+                i++;
+            } else {
+                ans += rmax - a[j];
+                j--;
             }
         }
         return ans;
